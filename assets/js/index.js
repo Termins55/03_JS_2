7e773452639bd661f4cc5c9d728f2a58e40004c1
Termins55 <kevgame123@gmail.com>
2025-08-05 21:04:47 +0300

    Add second ex

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,3 +23,25 @@ console.log(map1.has(2));
 // Отримайте список ключів та значень окремо.
 
 map1.forEach((value, key) => console.log(key, value));
+
+// 2. *Написати функцію, яка перероблятиме текст з числами
+// "This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents."
+//  на текст з порядковими числівниками за допомогою мапи вище:
+// "This year I will enter the first grade. I have two brothers. I am the third child of my parents."
+
+const map2 = new Map();
+map2.set("1", "first").set("3", "third");
+
+function test(numeral) {
+  return numeral
+    .split(" ")
+    .map((w) => (map2.has(w) ? map2.get(w) : w))
+    .join(" ");
+}
+
+const firstString =
+  "This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents.";
+
+const numeral = "first third";
+const convertedString = test(firstString);
+console.log(convertedString);
