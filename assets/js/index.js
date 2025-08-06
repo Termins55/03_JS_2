0577f1988cb19595e7900a399760f81da68ced36
Termins55 <kevgame123@gmail.com>
2025-08-06 15:44:48 +0300

    Add update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -32,7 +32,7 @@ map1.forEach((value, key) => console.log(key, value));
 const map2 = new Map();
 map2.set("1", "first").set("3", "third");
 
-function test(numeral) {
+function convertNumb(numeral) {
   return numeral
     .split(" ")
     .map((w) => (map2.has(w) ? map2.get(w) : w))
@@ -43,5 +43,5 @@ const firstString =
   "This year I will enter the 1 grade. I have two brothers. I am the 3 child of my parents.";
 
 const numeral = "first third";
-const convertedString = test(firstString);
+const convertedString = convertNumb(firstString);
 console.log(convertedString);
